68baa9cd5fa5d757e657094f91ec24dfd2904f22
Joabe Mendes <joab.mendes.r2d2@gmail.com>
2014-07-11 16:25:31 -0700

    Setup Database
    Setup Database

--- peteka/settings.py
+++ peteka/settings.py
@@ -57,8 +57,12 @@ WSGI_APPLICATION = 'peteka.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'jobr2d2_PETEKA',
+        'USER': 'jobr2d2_2261',
+        'PASSWORD': '22GI',
+        'HOST': '54.245.230.200',
+        'PORT': '3306'
     }
 }
 
