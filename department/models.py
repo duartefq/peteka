abb06b619790bd84512ec0ca132194a296ed50bb
Joabe Mendes <joab.mendes.r2d2@gmail.com>
2014-07-20 21:49:39 -0700

    unicode for models
    unicode for models

--- department/models.py
+++ department/models.py
@@ -6,6 +6,8 @@ class Employee(models.Model):
 	name = models.CharField(max_length="200")
 	email = models.CharField(max_length="200")
 	password = models.CharField(max_length="50")
+	def __unicode__(self):  # Python 3: def __str__(self):
+		return self.name
 
 class Supervisor_Employee(models.Model):
 	supervisor = models.ForeignKey(Employee, related_name='supervisor')
@@ -21,5 +23,7 @@ class Document(models.Model):
 	pdf = models.FileField(upload_to='pdfs')
 	responsible = models.ForeignKey(Employee, related_name='responsable')
 	requester = models.ForeignKey(Employee, related_name='requester')
+	def __unicode__(self):  # Python 3: def __str__(self):
+		return self.title
 
 
