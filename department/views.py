4effa6ecc52f6f1a685e28fd06ad91552d20eba7
Joabe Mendes <joab.mendes.r2d2@gmail.com>
2014-07-26 21:50:32 -0700

    View update
    Login screen
    Index method

--- department/views.py
+++ department/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from django.contrib.auth import authenticate, login #authentication lib
+
+from department.models import Employee, Document
+
+APP = 'department/'
+
+def index(request):
+	if request.POST:
+		username = request.POST["username"]
+		password = request.POST["password"]
+		return HttpResponse(username+"<br/>"+password)
+	else:
+		return render(request, APP+'login.html')
+	
--- peteka/urls.py
+++ peteka/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'peteka.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', include('department.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
